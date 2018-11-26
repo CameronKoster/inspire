a6bf4c0715398edc7ba5e099c4db2c653248273f
Cameron Koster <kostercam37@gmail.com>
2018-11-25 20:00:29 -0700

    can't get time to work

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ import WeatherController from "./components/weather/weather-controller.js"
 import ImageController from "./components/image/image-controller.js"
 import QuoteController from "./components/quote/quote-controller.js"
 import TodoController from "./components/todo/todo-controller.js"
-
+//import TimeController from "./components/time/time-controller.js"
 
 
 
@@ -13,9 +13,21 @@ class app {
       imageController: new ImageController(),
       quoteController: new QuoteController(),
       todoController: new TodoController(),
-
+      //timeController: new TimeController()
     }
   }
 }
+
+// function dateAndTime() {
+//   let currentDate = new Date()
+//   let currentTime = new Date()
+//   console.log(currentDate)
+//   let template = ""
+//   template += `
+//   <h1>${currentDate}</h1>
+//   <h2>${currentTime}</h2>
+//   `
+//   document.getElementById("time").innerHTML = template
+// }
 
 window.app = new app()  
